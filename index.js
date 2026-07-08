00b56995ebc4cb988fc1da1fe2fa4423611e8a15
kawsar9867 <kbosuniya@gmail.com>
2026-07-08 16:41:50 +0600

    Calculator Project Done

---
+++ index.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        if (display.value === "") return;
+        display.value = Function('"use strict"; return (' + display.value + ')')();
+    } catch {
+        display.value = "Error";
+    }
+}
